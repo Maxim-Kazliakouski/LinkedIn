14197e13130f79a64c246dd5d5b775399fc7527f
Max Kazliakouski <MKozlyakovskiy@alfabank.ru>
2025-02-19 11:36:03 +0300

    fix(): add screenshots

--- src/test/java/tests/CreatedConnectionsTest.java
+++ src/test/java/tests/CreatedConnectionsTest.java
@@ -4,10 +4,12 @@ import lombok.extern.log4j.Log4j2;
 import org.testng.annotations.Test;
 import tests.base.BaseWithStepsTest;
 
+import java.io.IOException;
+
 @Log4j2
 public class CreatedConnectionsTest extends BaseWithStepsTest {
     @Test
-    public void createNewConnections() {
+    public void createNewConnections() throws IOException {
         loginPageSteps
                 .goToLoginPage()
                 .login(getUsername(), getPassword());

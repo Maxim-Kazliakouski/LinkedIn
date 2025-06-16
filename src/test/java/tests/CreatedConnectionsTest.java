6fa3939e9f37039c3f60146b254a2ac7c5b05fb1
Max Kazliakouski <MKozlyakovskiy@alfabank.ru>
2025-06-16 20:07:49 +0300

    fix(): added scrollToBottom() method

--- src/test/java/tests/CreatedConnectionsTest.java
+++ src/test/java/tests/CreatedConnectionsTest.java
@@ -4,12 +4,10 @@ import lombok.extern.log4j.Log4j2;
 import org.testng.annotations.Test;
 import tests.base.BaseWithStepsTest;
 
-import java.io.IOException;
-
 @Log4j2
 public class CreatedConnectionsTest extends BaseWithStepsTest {
     @Test
-    public void createNewConnections() throws IOException {
+    public void createNewConnections() throws InterruptedException {
         loginPageSteps
                 .goToLoginPage()
                 .login(getUsername(), getPassword());
@@ -24,13 +22,15 @@ public class CreatedConnectionsTest extends BaseWithStepsTest {
 //                "hr", "HR", "Manager", "resource", "Resource", "UI/UX",
 //                "Recruiter", "recruiter", "testing"
 //        );
-//                .createConnection("Automation", "automation", "AQA", "aqa", "qa", "QA", "Qa", "Test Engineer", "PM", "Project Manager", "hr", "HR", "Manager", "resource", "Resource", "UI/UX", "Recruiter", "recruiter", "testing");
+//                .createConnection("Automation", "automation", "AQA", "aqa", "qa", "QA", "Qa", "Test Engineer", "PM", "Project Manager", "hr", "HR", "Manager", "manager", "resource", "Resource", "UI/UX", "Recruiter", "recruiter", "testing");
 
         /*
         new realization with list of specialities from Jenkins
         */
         String[] params = System.getProperty("specialitiesForAdding").split(", ");
+//        String[] params = new String[]{"Automation", "automation", "AQA", "aqa", "qa", "QA", "Qa", "Test Engineer", "PM", "Project Manager", "hr", "HR", "Manager", "manager", "resource", "Resource", "UI/UX", "Recruiter", "recruiter", "testing"};
         contactsPageSteps
+                .scrollToBottom()
                 .createConnection(params);
     }
 }

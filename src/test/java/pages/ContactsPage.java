6d789131fb10786c61fb32b22a5c9d398dbb0ff8
Max Kazliakouski <MKozlyakovskiy@alfabank.ru>
2025-06-16 20:05:50 +0300

    fix(): refactored ContactsPage

--- src/test/java/pages/ContactsPage.java
+++ src/test/java/pages/ContactsPage.java
@@ -1,39 +1,35 @@
 package pages;
 
 import lombok.extern.log4j.Log4j2;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static java.lang.String.format;
+import static java.lang.Thread.sleep;
 
 @Log4j2
 public class ContactsPage extends BasePage {
-    private static final Logger log = LoggerFactory.getLogger(ContactsPage.class);
-    private final By ALL_CONTACTS = By.xpath("//span[@class='discover-person-card__occupation t-14 t-black--light t-normal']");
-    @FindBy(xpath = "//section[@class='mn-community-summary']/div/h2")
+    private final By FIO = By.xpath("//span[@aria-hidden='true']");
+    @FindBy(xpath = "//p[text()='Manage my network']")
     WebElement titleContactPageMax;
     @FindBy(xpath = "//section[@class='mn-community-summary']/div")
     WebElement titleContactPageNatali;
-    @FindBy(xpath = "//div[@class='mn-community-summary__entity-info'][text()='Connections']//following-sibling::div[@class='pl3']")
+    @FindBy(xpath = "//a[contains(@aria-label,'connections')]/span[3]/span")
     WebElement amountOfConnectionsMax;
     @FindBy(xpath = "//div[@class='mn-community-summary__entity-info'][text()='Контакты']//following-sibling::div[@class='pl3']")
     WebElement amountOfConnectionsNatali;
-    private final String CONTACT_PAGE_URL = BASE_URL + "mynetwork/";
 
     public ContactsPage(WebDriver browser) {
         super(browser);
     }
 
     public void open() {
+        String CONTACT_PAGE_URL = BASE_URL + "mynetwork/";
         browser.get(CONTACT_PAGE_URL);
     }
 
@@ -42,84 +38,114 @@ public class ContactsPage extends BasePage {
         if (System.getProperty("usernameChrome").contains("nkazliakouskaya@gmail.com")) {
             return waitForVisibility(titleContactPageNatali);
         } else return waitForVisibility(titleContactPageMax);
+//        return waitForVisibility(titleContactPageMax);
     }
 
-    public WebElement getUserName(String spec) {
-        return browser.findElement(By.xpath(format(
-                "//span[text()='%s']//..//span[@class='discover-person-card__name t-16 t-black t-bold']", spec)));
+    public List<String> getUserNames() {
+        List<WebElement> nameElements = browser.findElements(FIO);
+        return nameElements.stream()
+                .filter(element -> !element.getText().contains("mutual"))
+                .map(WebElement::getText)
+                .map(String::trim)
+                .collect(Collectors.toList());
     }
 
     public String getWorkStatus(String userName) {
-        return browser.findElement(By.xpath(format("//span[text()='%s']//..//span[@class='discover-person-card__name t-16 t-black t-bold']//..//..//img", userName))).getAttribute("alt");
-    }
-
-    public boolean ghostPerson(String userName) {
-        boolean result = false;
         try {
-            System.out.println("!!!NOT GHOST PERSON --> CAN BE ADDED!!!");
-            if (!getWorkStatus(userName).contains("is open to work")) {
-                result = true;
-            }
-            return result;
-        } catch (Exception er) {
-            System.out.println("---> GHOST PERSON <--- CAN'T BE ADDED!!!");
-            By.xpath(format("//span[text()='%s']//..//span[@class='discover-person-card__name t-16 t-black t-bold']//..//div[contains(@class,'ghost-entity')]", userName));
-            return false;
+            WebElement element = browser.findElement(By.xpath(format("//span[text()=\"%s\"]//..//..//..//div[1]", userName)));
+            String attribute = element.getAttribute("aria-label");
+            return attribute != null ? attribute : "";
+        } catch (NoSuchElementException e) {
+            return "";
         }
     }
 
-    public By getButtonBySpecialization(String spec) {
-        return By.xpath(format("//span[@class='discover-person-card__occupation t-14 t-black--light t-normal'][text()='%s']//..//..//..//footer//button", spec));
+    public Map<String, String> getSpecializationMapByFio() {
+        Map<String, String> nameToSpecializationMap = new HashMap<>();
+        List<String> userNames = getUserNames(); // Сохраняем результат в переменную
+
+        for (String userName : userNames) {
+            System.out.println(userName);
+            try {
+                WebElement specializationElement = browser.findElement(
+                        By.xpath(format("//span[text()=\"%s\"]//following::p[1]", userName))
+                );
+
+                String specializationText = specializationElement.getText();
+                String workStatus = getWorkStatus(userName);
+
+                if (workStatus != null && workStatus.contains("open to work")) {
+                    log.info("This contact {} is open to work", userName);
+                }
+
+                boolean isNotJunior = !specializationText.toLowerCase().contains("junior");
+                boolean isNotOpenToWork = workStatus == null || !workStatus.contains("open to work");
+
+                if (isNotJunior && isNotOpenToWork) {
+                    nameToSpecializationMap.put(userName, specializationText);
+                }
+
+            } catch (NoSuchElementException e) {
+                log.error("Element not found for user: {}", userName);
+            }
+        }
+
+        return nameToSpecializationMap;
     }
 
-    public WebElement pendingButton(String spec) {
-        return browser.findElement(By.xpath(format("//span[text()='%s']//following::span[text()='Pending']", spec)));
+    public void inviteAllFromMap(Map<String, String> nameToSpecializationMap) {
+        if (nameToSpecializationMap == null || nameToSpecializationMap.isEmpty()) {
+            log.info("Мапа пустая или null. Нет контактов для приглашения.");
+            return;
+        }
+
+        for (String name : nameToSpecializationMap.keySet()) {
+            try {
+
+                // Формируем локатор для кнопки приглашения
+                By inviteButtonLocator = By.xpath(String.format("//button[@aria-label=\"Invite %s to connect\"]", name));
+
+                // Ищем кнопку и кликаем
+                WebElement inviteButton = browser.findElement(inviteButtonLocator);
+//                inviteButton.click();
+
+                log.info("Отправлено приглашение для: {}", name);
+
+                // Можно добавить задержку, если нужно (например, для стабильности)
+//                sleep(1000);
+
+            } catch (NoSuchElementException e) {
+                log.error("❌ Не найдена кнопка приглашения для: {}, т.к. есть кнопка 'Follow'", name);
+            } catch (Exception e) {
+                log.error("⚠️ Ошибка при обработке контакта: {} -> {}", name, e.getMessage());
+            }
+        }
     }
 
-    public String connectionsAmount() {
+    public String getConnectionsAmount() {
         if (System.getProperty("usernameChrome").contains("nkazliakouskaya@gmail.com")) {
             return amountOfConnectionsNatali.getText();
         } else return amountOfConnectionsMax.getText();
+//        return amountOfConnectionsMax.getText();
     }
 
     public void createConnectionAndGetAmountOfConnections(String... searchingSpecialists) {
-        ArrayList<String> desiredContacts = new ArrayList<>();
-        for (String param : searchingSpecialists) {
-            desiredContacts.add(param);
-        }
-        List<WebElement> contactQaList = browser.findElements(ALL_CONTACTS);
-        ArrayList<String> contacts = new ArrayList<>();
-        ArrayList<String> contactsForClick = new ArrayList<>();
-        for (WebElement eachContact : contactQaList) {
-            contacts.add(eachContact.getText());
-        }
-        Set<String> uniqueContacts = new HashSet<>(contacts);
-        for (String specialization : uniqueContacts) {
-            for (String eachUniqueContact : desiredContacts) {
-                if (specialization.contains(eachUniqueContact)
-                        && !specialization.contains("Junior")
-                        && !specialization.contains("junior")
-                        && ghostPerson(specialization)) {
-//                        && !getWorkStatus(specialization).contains("is open to work"))
-                    contactsForClick.add(specialization);
-                    try {
-//                        log.info(getWorkStatus(getUserName(specialization).getText()));
-                        clickJS(getButtonBySpecialization(specialization));
-                        waitForElementClickable(pendingButton(specialization));
-                        log.info(getUserName(specialization).getText() + " --> " + specialization + " -- has been added");
-                    } catch (Exception er) {
-                        log.error("The contact '" + getUserName(specialization).getText() + "' hasn't been added \nor 'Pending button hasn't been shown'");
-                    }
-                }
-            }
-        }
-//        System.out.println(contactsForClick);
-        if (contactsForClick.size() == 0) {
-            log.warn("There is no any contacts for adding");
-        } else {
-            log.info(format("The %s contact(s) has been added", contactsForClick.size()));
-            log.info(format("Amount of connections in given moment: %s", connectionsAmount()));
-        }
+        Map<String, String> specializationMap = getSpecializationMapByFio(); // Сохраняем результат в переменную
+
+        inviteAllFromMap(specializationMap);
+
+        // Логирование результатов
+        specializationMap.forEach((key, value) ->
+                log.info("{} → {}", key, value));
+
+        log.info("The {} contact(s) has been added", specializationMap.size());
+        log.info("Amount of connections at the moment: {}", getConnectionsAmount());
+    }
+
+    public void scrollToBottomOfThePage() throws InterruptedException {
+        browser.findElement(By.xpath("(//p[contains(text(),'People you may know')])[1]")).click();
+        sleep(5000);
+        browser.findElement(By.tagName("body")).sendKeys(Keys.END);
+        sleep(5000);
     }
 }
-//}
